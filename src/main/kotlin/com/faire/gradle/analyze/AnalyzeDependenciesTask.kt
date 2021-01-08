7e0f2940eb953efe38c7e50ed086f44258e3e4a9
Ben Poland <ben.poland@faire.com>
2021-01-08 09:01:31 -0500

    Improve error messages
    - also fix some inconsistent indentation

--- src/main/kotlin/com/faire/gradle/analyze/AnalyzeDependenciesTask.kt
+++ src/main/kotlin/com/faire/gradle/analyze/AnalyzeDependenciesTask.kt
@@ -8,7 +8,7 @@ import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.TaskAction
 
-open class DependencyAnalysisException(message: String) : GradleException(message) {}
+open class DependencyAnalysisException(message: String) : GradleException(message)
 
 open class AnalyzeDependenciesTask : DefaultTask() {
   @InputFiles
@@ -24,8 +24,10 @@ open class AnalyzeDependenciesTask : DefaultTask() {
   var includedPackageFilters: List<String> = listOf()
 
   private fun applyAnalysisResultToBuffer(
-      outputBuffer: StringBuffer,
-      problemArtifacts: Set<ResolvedArtifact>, problemLabel: String) {
+    outputBuffer: StringBuffer,
+    problemArtifacts: Set<ResolvedArtifact>,
+    problemLabel: String
+  ) {
     val filteredProblems = if (includedPackageFilters.isEmpty()) {
       problemArtifacts
     } else {
@@ -40,7 +42,7 @@ open class AnalyzeDependenciesTask : DefaultTask() {
       outputBuffer.append("$problemLabel: \n")
       filteredProblems.forEach { artifact ->
         val classifier = artifact.classifier ?: ""
-        outputBuffer.append(" - ${artifact.moduleVersion}${classifier}@${artifact.extension}\n")
+        outputBuffer.append(" - ${artifact.moduleVersion}$classifier@${artifact.extension}\n")
       }
     }
   }
@@ -50,25 +52,45 @@ open class AnalyzeDependenciesTask : DefaultTask() {
     val analysis = ProjectDependencyResolver(project, mainClassesDirs, testClassesDirs).analyzeDependencies()
     val buffer = StringBuffer()
 
-    applyAnalysisResultToBuffer(buffer, analysis.mainUsedUndeclaredArtifacts, "mainUsedUndeclaredArtifacts")
-    applyAnalysisResultToBuffer(buffer, analysis.testUsedUndeclaredArtifacts, "testUsedUndeclaredArtifacts")
     applyAnalysisResultToBuffer(
-        buffer,
-        analysis.mainUnusedDeclaredButUsedByTest,
-        "mainUnusedDeclaredButUsedByTest"
-    )
-    applyAnalysisResultToBuffer(buffer, analysis.mainUnusedDeclaredArtifacts, "mainUnusedDeclaredArtifacts")
-    applyAnalysisResultToBuffer(buffer, analysis.testUnusedDeclaredArtifacts, "testUnusedDeclaredArtifacts")
-    applyAnalysisResultToBuffer(buffer, analysis.testUnnecessaryDeclarations, "testUnnecessaryDeclarations")
-    applyAnalysisResultToBuffer(
-        buffer,
-        analysis.mainUnnecessaryPermitUnusedDeclaredArtifacts,
-        "mainUnnecessaryPermitUnusedDeclaredArtifacts"
+      buffer,
+      analysis.mainUsedUndeclaredArtifacts,
+      MAIN_USED_UNDECLARED_MSG
     )
     applyAnalysisResultToBuffer(
-        buffer,
-        analysis.testUnnecessaryPermitUnusedDeclaredArtifacts,
-        "testUnnecessaryPermitUnusedDeclaredArtifacts"
+      buffer,
+      analysis.testUsedUndeclaredArtifacts,
+      TEST_USED_UNDECLARED_MSG
+    )
+    applyAnalysisResultToBuffer(
+      buffer,
+      analysis.mainUnusedDeclaredButUsedByTest,
+      MAIN_UNUSED_DECLARED_BUT_USED_BY_TEST_MSG
+    )
+    applyAnalysisResultToBuffer(
+      buffer,
+      analysis.mainUnusedDeclaredArtifacts,
+      MAIN_UNUSED_DECLARED_MSG
+    )
+    applyAnalysisResultToBuffer(
+      buffer,
+      analysis.testUnusedDeclaredArtifacts,
+      TEST_UNUSED_DECLARED_MSG
+    )
+    applyAnalysisResultToBuffer(
+      buffer,
+      analysis.testUnnecessaryDeclarations,
+      TEST_UNNECESSARY_DECLARATIONS_MSG
+    )
+    applyAnalysisResultToBuffer(
+      buffer,
+      analysis.mainUnnecessaryPermitUnusedDeclaredArtifacts,
+      MAIN_UNNECESSARY_PERMIT_UNUSED_DECLARED_MSG
+    )
+    applyAnalysisResultToBuffer(
+      buffer,
+      analysis.testUnnecessaryPermitUnusedDeclaredArtifacts,
+      TEST_UNNECESSARY_PERMIT_UNUSED_DECLARED_MSG
     )
 
     if (!buffer.isEmpty()) {
@@ -79,5 +101,22 @@ open class AnalyzeDependenciesTask : DefaultTask() {
         throw DependencyAnalysisException(message)
       }
     }
+  }
+
+  companion object {
+    const val MAIN_USED_UNDECLARED_MSG = "Main dependencies used, but not declared in gradle"
+    const val TEST_USED_UNDECLARED_MSG = "Test dependencies used, but not declared in gradle"
+    const val MAIN_UNUSED_DECLARED_BUT_USED_BY_TEST_MSG = "Main dependencies not used, but are used by test" +
+            " -- e.g. change implementation() to testImplementation() in build.gradle.kts"
+    const val MAIN_UNUSED_DECLARED_MSG = "Main dependencies not used, but declared in gradle" +
+            " -- remove implementation()/api() references in build.gradle.kts"
+    const val TEST_UNUSED_DECLARED_MSG = "Test dependencies not used, but declared in gradle" +
+            " -- remove testImplementation()/testApi() references in build.gradle.kts"
+    const val TEST_UNNECESSARY_DECLARATIONS_MSG = "Test dependencies already declared by main" +
+            " -- remove testImplementation()/testApi() references in build.gradle.kts"
+    const val MAIN_UNNECESSARY_PERMIT_UNUSED_DECLARED_MSG = "Main dependency is listed as permitUnusedDeclared()" +
+            " but does not need to be -- remove its permitUnusedDeclared() configuration in build.gradle.kts"
+    const val TEST_UNNECESSARY_PERMIT_UNUSED_DECLARED_MSG = "Test dependency is listed as permitTestUnusedDeclared()" +
+            " but does not need to be -- remove its permitTestUnusedDeclared() configuration in build.gradle.kts"
   }
 }
