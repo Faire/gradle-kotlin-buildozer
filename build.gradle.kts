bc7e27ebc4e055e69a4f225012a30b4e1c17d95a
Ryan Fioravanti <ryan@faire.com>
2021-11-30 13:35:20 -0500

    Fixing issue with directories having no build file. V 1.0.7 -> 1.0.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 allprojects {
   group = "com.faire.gradle"
-  version = "1.0.7"
+  version = "1.0.8"
 
   repositories {
     mavenCentral()
--- src/main/kotlin/com/faire/gradle/analyze/AnalyzeDependenciesTask.kt
+++ src/main/kotlin/com/faire/gradle/analyze/AnalyzeDependenciesTask.kt
@@ -8,6 +8,7 @@ import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.PathSensitive
 import org.gradle.api.tasks.PathSensitivity
@@ -27,6 +28,16 @@ open class AnalyzeDependenciesTask : DefaultTask() {
 
   // Using the buildFile as an input so that if you change any dependencies we invalidate analysis cache.
   @InputFile
+  // Sometimes gradle thinks there is a project at a location with no build file. Use optional here to make sure
+  // it doesn't fail in those cases. It would probably be better to figure out why this was happening, but there's
+  // no time for that!
+  // The situation I had was as follows
+  //    foo (no build file here)
+  //    foo/bar1 (has build file)
+  //    foo/bar2 (has build file)
+  //
+  // Running ./gradlew analyzeDependencies would fail at :foo:analyzeDependencies
+  @Optional
   @PathSensitive(PathSensitivity.RELATIVE)
   var buildFile = project.buildFile
 
